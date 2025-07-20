3d41c205c7925a56e054d6010af125cc750c0121
yarenYLDZ <yarenyldz654@gmail.com>
2025-07-20 16:51:23 +0300

    Hafta 1 in 5 odevi

--- src/Week_1/CinZodyagiHesaplama/CinZodyagiHesaplama.java
+++ src/Week_1/CinZodyagiHesaplama/CinZodyagiHesaplama.java
@@ -11,20 +11,22 @@ public class CinZodyagiHesaplama {
         int kalan = dogumYili % 12;
         String burc = "";
 
-        switch (kalan){
-            case 0: burc = "Maymun"; break;
-            case 1: burc = "Horoz"; break;
-            case 2: burc = "Köpek"; break;
-            case 3: burc = "Domuz"; break;
-            case 4: burc = "Fare"; break;
-            case 5: burc = "Öküz"; break;
-            case 6: burc = "Kaplan"; break;
-            case 7: burc = "Tavşan"; break;
-            case 8: burc = "Ejderha"; break;
-            case 9: burc = "Yılan"; break;
-            case 10: burc = "At"; break;
-            case 11: burc = "Koyun"; break;
-        }
+        burc = switch (kalan) {
+            case 0 -> "Maymun";
+            case 1 -> "Horoz";
+            case 2 -> "Köpek";
+            case 3 -> "Domuz";
+            case 4 -> "Fare";
+            case 5 -> "Öküz";
+            case 6 -> "Kaplan";
+            case 7 -> "Tavşan";
+            case 8 -> "Ejderha";
+            case 9 -> "Yılan";
+            case 10 -> "At";
+            case 11 -> "Koyun";
+            default -> "Bilinmiyor"; // Güvenlik amaçlı eklenebilir
+        };
+
 
         System.out.println("Çin Zodyağı Burcunuz : " + burc);
         scanner.close();
