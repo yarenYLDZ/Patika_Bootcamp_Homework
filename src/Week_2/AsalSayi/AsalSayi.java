15f7a4e43e4a04a2e8863e707bb95ded8c50e615
yarenYLDZ <yarenyldz654@gmail.com>
2025-07-29 00:10:07 +0300

    ters ucgen olusturuldu

---
+++ src/Week_2/AsalSayi/AsalSayi.java
@@ -0,0 +1,45 @@
+package Week_2.AsalSayi;
+
+import java.util.Scanner;
+
+public class AsalSayi {
+    public static void main(String[] args) {
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.println("Lutfen sayi giriniz: ");
+        int girilenSayi = scanner.nextInt();
+
+        if (girilenSayi < 2) {
+            System.out.println(girilenSayi + " asal değildir !");
+        } else {
+            boolean sonuc = asalMi(girilenSayi, 2);
+
+            if (sonuc) {
+                System.out.println(girilenSayi + " sayısı asaldir !");
+            } else {
+                System.out.println(girilenSayi + " sayısı asal değildir !");
+            }
+        }
+
+        scanner.close();
+
+
+
+    }
+
+
+    private static boolean asalMi(int sayi, int bolen) {
+        // Eğer bolen sayıya eşitse, bölünebilirlik bulunamadı, asal sayıdır
+        if (bolen == sayi) {
+            return true;
+        }
+
+        // Eğer sayı bolene tam bölünüyorsa, asal değildir
+        if (sayi % bolen == 0) {
+            return false;
+        }
+
+        // Bir sonraki bölenle devam et
+        return asalMi(sayi, bolen + 1);
+    }
+}
---
+++ src/Week_2/AsalSayi/ReadMe.md
@@ -0,0 +1,12 @@
+Recursive Yöntemle Asal Sayı Kontrolü
+Bu program, kullanıcıdan alınan bir sayının asal olup olmadığını recursive (özyinelemeli) yöntemle kontrol eder. Sayı 2’den başlayarak kendisine kadar olan bölenlerle denenir ve bölünebilirlik durumu sorgulanır. Sonuç ekranda kullanıcıya bildirilir.
+
+
+
+
+
+
+
+
+
+ChatGPT’ye sor
