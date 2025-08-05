dc654186e451e1b4ac15386c6732f8aed965e0d6
yarenYLDZ <yarenyldz654@gmail.com>
2025-08-05 11:40:33 +0300

    en yakin iki sayi bulma odevi

---
+++ src/Week_3/HarfBulmaOyunu/HarfBulmaOyunu.java
@@ -0,0 +1,41 @@
+package Week_3.HarfBulmaOyunu;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class HarfBulmaOyunu {
+    public static void main(String[] args) {
+        ArrayList<String> harfListesi = new ArrayList<>();
+        Random random = new Random();
+        Scanner scanner=new Scanner(System.in);
+
+        for (int i = 0; i < 4; i++) {
+            char rastgeleHarf = (char) (random.nextInt(26) + 'a'); // a-z
+            harfListesi.add(String.valueOf(rastgeleHarf));
+        }
+
+        System.out.println("Başlangıç Listesi: " + harfListesi);
+
+        // Kullanıcıdan 4 kez harf girişi alınıyor
+        for (int i = 0; i < 4; i++) {
+            System.out.print((i + 1) + ". harfi giriniz: ");
+            String girilenHarf = scanner.next().toLowerCase();
+
+            // Harf listede varsa "found" ile değiştir
+            if (harfListesi.contains(girilenHarf)) {
+                int index = harfListesi.indexOf(girilenHarf);
+                harfListesi.set(index, "found");
+                System.out.println("Tebrikler! Harfi buldunuz.");
+            } else {
+                harfListesi.add(girilenHarf);
+                System.out.println("Harf listede yoktu, eklendi.");
+            }
+        }
+
+        // Son listeyi yazdır
+        System.out.println("Güncellenmiş Liste: " + harfListesi);
+
+
+    }
+}
